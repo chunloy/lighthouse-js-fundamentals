1e28a7be214a5b3b7a2d2ec87243a8a24ae56cc8
Aaron Au <aaron.au@me.com>
2022-03-22 03:46:59 +0000

    modified code for temperature conditional

--- conditionals.js
+++ conditionals.js
@@ -12,10 +12,27 @@ if (cold) {
 console.log('Now you\'re ready to go outside!');
 */
 
-const cold = false;
+//-------------------------------------------------
+
+/*const cold = false;
 
 if (cold) {
   console.log('Make sure you pick out a scarf!');
 } else {
   console.log('Short sleeves are fine.');
 }
+*/
+
+//-------------------------------------------------
+
+const temperature = 20;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("short sleeves won't cut it!");
+} else {
+  console.log("short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
