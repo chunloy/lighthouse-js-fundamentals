e39b4bc68bb6a482cf12af382ce245ff6d401a5d
Aaron Au <aaron.au@me.com>
2022-03-23 00:09:11 +0000

    function that prints hello with argument

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*const sayHello = function () {
+  console.log("Hello, world");
+  console.log("This is an anonymous function.");
+}
+*/
+//----------------------------------------------
+const sayHello = function (name) {
+  console.log("Hello, " + name)
+}
+sayHello("world");
+sayHello("everybody");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
