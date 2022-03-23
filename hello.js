fb8ee4a1741018c094e425716e8a366a4cb171f3
Aaron Au <aaron.au@me.com>
2022-03-23 00:12:51 +0000

    modified code to return a string instead of printing

--- hello.js
+++ hello.js
@@ -3,7 +3,10 @@
   console.log("This is an anonymous function.");
 }
 */
+
 //----------------------------------------------
+
+/*
 const sayHello = function (name) {
   console.log("Hello, " + name)
 }
@@ -12,3 +15,19 @@ sayHello("everybody");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+//----------------------------------------------
+
+/*
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
